0176a67f27856ca59f42b44325a8fa219d27e43d
Mackenzzy <mackenzzy@gmail.com>
2023-10-11 15:37:49 -0300

    Fontes

--- src/componentes/EstilosGlobais/index.jsx
+++ src/componentes/EstilosGlobais/index.jsx
@@ -1,25 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 
+import GandhiSansRegular from './fontes/GandhiSans-Regular.otf'
+
+import GandhiSansBold from './fontes/GandhiSans-Bold.otf';
+
+
 const EstilosGlobais = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Italiana&family=Montserrat:wght@100;200;300;400;600&display=swap');
 
-:root {
-  --font-italiana: 'Italiana', cursive;
-  --font-montserrat: 'Montserrat', sans-serif;
+@font-face {
+  font-family: 'GandhiSansRegular';
+  src: local('Gandhi Sans Regular'), local('GandhiSansRegular'), url(${GandhiSansRegular});
 }
 
-body {
-  font-family: var(--font-montserrat);
-}
 
-h1 {
-  font-family: var(--font-italiana);
+@font-face {
+  font-family: 'GandhiSansBold';
+  src: local('Gandhi Sans Bold'), local('GandhiSansBold'), url(${GandhiSansBold});
 }
 
 
 html {
   line-height: 1.15; 
   -webkit-text-size-adjust: 100%; 
+
 }
 body {
   margin: 0;
