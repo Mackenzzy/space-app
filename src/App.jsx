11f3ca8cb6d58c18418a7692cca345b1884e2c5b
Mackenzzy <mackenzzy@gmail.com>
2023-10-11 20:40:24 -0300

    Criando a galeria 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,51 @@ import EstilosGlobais from './componentes/EstilosGlobais/index'
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/Banner"
+import BannerBackground from "./assets/Banner.png"
+import Galeria from "./componentes/Galeria"
 
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width:100%;
-  min-height: 100vh;
-  
+  min-height: 100vh;  
   `
+
+const AppContainer = styled.div`
+  width: 1440px;
+  max-width: 100%;
+  margin: 0 auto;
+`
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+const ConteudodaGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 
 function App() {
 
   return (
     <FundoGradiente>
+      
       <EstilosGlobais/>
+      <AppContainer>
 
-      <Cabecalho/>
-      <BarraLateral/>
-      <Banner/>
-
+        <Cabecalho/>
+        <MainContainer>
+          <BarraLateral/>
+          <ConteudodaGaleria>
+            <Banner
+              texto="A Galeria mais completa de fotos do espaço!"
+              backgroundImage= {BannerBackground}
+            />
+            <Galeria/>
+          </ConteudodaGaleria>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
   )
 }
