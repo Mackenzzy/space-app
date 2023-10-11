c21bf2e7facd0311cf6c8c46fc1382efc459ac38
Mackenzzy <mackenzzy@gmail.com>
2023-10-10 22:35:36 -0300

    Adicionas as props dos itens de navegacao

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -4,42 +4,55 @@ import ItemNavegacao from "./ItemNavegacao";
 const BarraLateralEstilizado = styled.div`
 
 `
-const listaEstilizada = styled.ul`
-    
-`
+const ListaEstilizada = styled.ul`
+    text-decoration: none;
+    list-style-type: none;
+`;
 const ItemEstilizado = styled.li`
 `
 const BarraLateral = () => {
+    
    return(
    <aside>
         <nav>
-            <listaEstilizada>
+            <ListaEstilizada>
                 <ItemNavegacao
                     iconeAtivo="./icones/home-ativo.png"
                     iconeInativo="./icones/Home-inativo.png"
                     ativo={true}
-                />
+                    >
+                        Inicio
+                </ItemNavegacao>
+                
                 <ItemNavegacao
                     iconeAtivo="./icones/mais-vistas-ativo.png"
                     iconeInativo="./icones/mais-vistas-inativo.png"
                     ativo={false}
-                />
+                    >
+                        Mais Vistas
+                </ItemNavegacao>
                 <ItemNavegacao
                     iconeAtivo="./icones/mais-curtidas-ativo.png"
                     iconeInativo="./icones/mais-curtidas-inativo.png"
                     ativo={false}
-                />
+                    >
+                        Mais Curtidas
+                </ItemNavegacao>
                 <ItemNavegacao
                     iconeAtivo="./icones/novas-ativo.png"
                     iconeInativo="./icones/novas-inativo.png"
                     ativo={false}
-                />
+                    >
+                        Novas
+                </ItemNavegacao>
                 <ItemNavegacao
                     iconeAtivo="./icones/surpreenda-me-ativo.png"
                     iconeInativo="./icones/surpreenda-me-inativo.png"
                     ativo={false}
-                />
-            </listaEstilizada>
+                    >
+                        Surpreenda-me
+                </ItemNavegacao>
+            </ListaEstilizada>
         </nav>
     </aside>
     )
