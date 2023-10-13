4eeaa0346bb75e4a1e11cf931d8fcd5b8cf45199
Mackenzzy <mackenzzy@gmail.com>
2023-10-13 20:43:46 -0300

    Tags criadas

--- src/componentes/Galeria/tags/index.jsx
+++ src/componentes/Galeria/tags/index.jsx
@@ -1,4 +1,16 @@
+import tags from './tags.json'
+import styled from 'styled-components'
+
+const BotaoTag = styled.button``
+
 const Tags = () => {
-    return <p>Tags</p>
+    return <>
+
+        <p>Busque por tags:</p>
+        {tags.map((tag) => (
+            <button key={tag.id}>{tag.titulo}</button>
+        ))}
+
+    </>
 }
 export default Tags
