6b04025f46cf9a5351a3015a773380209e39c6ed
Mackenzzy <mackenzzy@gmail.com>
2023-10-13 21:22:18 -0300

    Estilizando as tags

--- src/componentes/Galeria/tags/index.jsx
+++ src/componentes/Galeria/tags/index.jsx
@@ -1,16 +1,46 @@
-import tags from './tags.json'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import tags from './tags.json';
+import styled from 'styled-components';
 
-const BotaoTag = styled.button``
+const SecaoTags = styled.section`
+  display: flex;
+  gap: 24px;
+  color: #fff;
+  font-family: 'GandhiSansRegular';
+  font-size: 24px;
+  padding: 56px 0px;
+`;
+
+const BotaoTag = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }`
 
 const Tags = () => {
-    return <>
-
-        <p>Busque por tags:</p>
-        {tags.map((tag) => (
-            <button key={tag.id}>{tag.titulo}</button>
-        ))}
-
-    </>
+  const [Botao, setBotao] = useState(0);
+  const trocarCorBorda = (id) => {
+    setBotao(id)
 }
-export default Tags
+
+  return (
+    <SecaoTags>
+      <p>Busque por tags:</p>
+      {tags.map(tag => 
+      <BotaoTag $ativo={tag.id === Botao} key={tag.id} onClick={
+        () => trocarCorBorda(tag.id)}>{tag.titulo}
+        </BotaoTag>)}
+     
+    </SecaoTags>
+  );
+};
+
+export default Tags;
