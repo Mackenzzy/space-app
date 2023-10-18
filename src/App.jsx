9dfdf24e1341c9bdf8e913d74f4d401b87641950
Mackenzzy <mackenzzy@gmail.com>
2023-10-17 23:55:12 -0300

    Implementando o botão de favoritar

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,12 @@ const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
   const [fotoSelecionada, setFotoSelecionada] = useState(null)
   const aoAlternarFavorito = (foto) => {
-    console.log(foto)
+    setFotosDaGaleria(fotosDaGaleria.map(fotoDaGaleria => {
+      return{
+        ...fotoDaGaleria,
+        favorita: fotoDaGaleria.id === foto.id ? !foto.favorita : fotoDaGaleria.favorita
+      }
+    }))
   }
   
   return (
--- src/componentes/Galeria/imagem/index.jsx
+++ src/componentes/Galeria/imagem/index.jsx
@@ -49,6 +49,8 @@ const Rodape = styled.footer`
 `
 
 const Imagem = ({ foto, expandida = false, aoZoomSolicitado, aoAlternarFavorito }) => {
+    const iconeFavorita = foto.favorita ? '/icones/favorito-ativo.png' : '/icones/favorito.png';
+
     return (
         <Galeria>
             <Figure $expandida={expandida} id={`foto-${foto.id}`}>
@@ -58,7 +60,7 @@ const Imagem = ({ foto, expandida = false, aoZoomSolicitado, aoAlternarFavorito 
                     <Rodape>
                         <h4>{foto.fonte}</h4>
                         <BotaoIcone onClick={() => aoAlternarFavorito(foto)}>
-                            <img src="/icones/favorito.png" alt="Icone de favorito" />
+                            <img src={iconeFavorita} alt="Icone de favorito" />
                         </BotaoIcone>
                         {!expandida && <BotaoIcone aria-hidden={expandida} onClick={() => aoZoomSolicitado(foto)}>
                             <img src="/icones/expandir.png" alt="Icone de expandir" />
