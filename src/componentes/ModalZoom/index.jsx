dfadfb42a9a84a05c0849c30fa02234b06f83636
Mackenzzy <mackenzzy@gmail.com>
2023-10-17 23:21:30 -0300

    Criando a categoria Populares

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,15 +1,41 @@
+import Imagem from '../Galeria/imagem'
+import styled from 'styled-components'
 
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`
+const DialogEstilizado = styled.dialog`
+    position: absolite;
+    top: 294px;
+`
 
 const ModalZoom = ({foto}) => {
 
     return (
         <>
-            <dialog open = {!!foto}>
+            {foto && <>
+            <Overlay/>
+                <DialogEstilizado open = {!!foto}>
+                <Imagem
+                    foto={foto}
+                    expandida = {true}
+                />
+
                 <p>Exemplo do Mozilla</p>
                 <form method="dialog">
                     <button type="submit">ok</button>
                 </form>
-            </dialog>
+            </DialogEstilizado>
+            </>}
 
         </>
     )
